bd634289f97cbf9df0cf7325f09374e43df2ac38
Ahmed Bassam <ahmedbassam513@gmail.com>
2022-12-12 15:28:10 +0200

    added mux8

--- coding/Mux21.sv
+++ coding/Mux21.sv
@@ -1,12 +1,13 @@
 module  Mux21 (
-    D,S,Y
+    Y,D,S
 );
     input logic D[0:1];
     input logic S;
     output logic Y;
-    wire Sn,X1,X2;
+    wire Sn;
     not(Sn,S);
+	 wire X1,X2;
     and(X1,Sn,D[0]);
     and(X2,S,D[1]);
-    or(X1,X2);
+    or(Y,X1,X2);
 endmodule
